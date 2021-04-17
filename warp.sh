ca045446ae50f408f7729e69f2894858edc2a13a
Vincent Young <u@nisekoo.com>
2021-04-17 19:29:02 +0800

    Update warp.sh

--- warp.sh
+++ warp.sh
@@ -14,6 +14,46 @@ plain='\033[0m'
 # Make sure only root can run our script
 [[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] This script must be run as root!" && exit 1
 
+# 判断系统版本
+check_system(){
+	if [[ -f /etc/redhat-release ]]; then
+	release="centos"
+	elif grep -Eqi "debian|raspbian" /etc/issue; then
+		release="debian"
+	elif grep -Eqi "ubuntu" /etc/issue; then
+		release="ubuntu"
+	elif grep -Eqi "centos|red hat|redhat" /etc/issue; then
+		release="centos"
+	elif grep -Eqi "debian|raspbian" /proc/version; then
+		release="debian"
+	elif grep -Eqi "ubuntu" /proc/version; then
+		release="ubuntu"
+	elif grep -Eqi "centos|red hat|redhat" /proc/version; then
+		release="centos"
+	fi
+
+# 根据系统安装必要软件
+	if [ $release = "centos" ]
+	then
+		yum install kmod-wireguard wireguard-tools -y
+	elif [ $release = "debian" ]
+	then
+		apt-get install sudo net-tools openresolv -y
+		echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable-wireguard.list
+		printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' > /etc/apt/preferences.d/limit-unstable
+		apt-get update
+		sudo apt-get install linux-headers-`uname -r` -y
+		apt-get install wireguard-dkms wireguard-tools -y
+	elif [ $release = "ubuntu" ]
+	then
+		apt-get install sudo net-tools openresolv -y
+		apt-get update
+		apt-get install wireguard -y
+	else
+		echo "不支持该系统"
+		exit 1
+	fi
+}
 
 get_char(){
 	SAVEDSTTY=`stty -g`
@@ -25,14 +65,14 @@ get_char(){
 	stty $SAVEDSTTY
 }
 
-	echo "---------------------------------------------"
+	echo "---------------------------------------------------------"
 	echo
 	echo -e "${green}Cloudflare Warp 一键脚本 Made by Vincent${plain}"
 	echo
-	echo -e "${red}支持Debian任意系列和CentOS Kernel >= 5.4${plain}"
+	echo -e "${red}支持Debian、Ubuntu、CentOS${plain}"
 	echo
 	echo -e "${red}请先使用 uname -r 检查你的内核版本${plain}"
-	echo "---------------------------------------------"
+	echo "---------------------------------------------------------"
 	echo
 	echo "按任意键继续，或者按Control + C 退出"
 	char=`get_char`
@@ -42,31 +82,30 @@ get_char(){
 	echo -e "开始下载必要文件"
 	wget -O wgcf https://stern.codes/wgcf_2.1.4_linux_amd64
 	chmod +x wgcf
-	echo "开始注册WGCF"
+	echo "开始注册Warp Account"
 	./wgcf register
 	./wgcf generate
 	echo "已完成注册，即将执行下一步操作"
+	echo
 
 # 选择启动模式
 	echo "------------------------------------------"
 	echo
-	echo "1: 为VPS增加Warp IPV6"
+	echo "1: 增加Warp IPV6"
 	echo
-	echo "2: 为VPS增加Warp IPV4"
+	echo "2: 增加Warp IPV4"
 	echo
-	echo "3: 为VPS增加Warp IPV4和IPV6"
+	echo "3: 增加Warp IPV4 & IPV6"
 	echo
 	echo "------------------------------------------"
-	read -p "请选择你需要启动的模式" start_type
+	read -p "请选择你需要启动的模式: " start_type
 	case $start_type in
 	1)
 		clear
 		echo
 		echo "你选择了增加IPV6的模式"
 		echo
-			
 		echo "开始为该模式配置"
-		echo
 		sed -i '/AllowedIPs = 0.0.0.0/d'  wgcf-profile.conf
 		sed -i 's/'engage.cloudflareclient.com'/'162.159.192.1'/g' wgcf-profile.conf
 		echo
@@ -74,8 +113,9 @@ get_char(){
 		echo
 		echo "开始安装WireGuard"
 		echo 
-		yum install kmod-wireguard wireguard-tools -y
-		echo 
+		
+		check_system
+		echo
 		echo "安装完成，开始加载WireGuard内核模块"
 		modprobe wireguard
 		echo 
@@ -90,18 +130,15 @@ get_char(){
 		echo
 		echo "恭喜你！配置完成！"
 		echo
-		echo "Made by Vincent"
+		echo "感谢使用!"
 		echo
-		echo "Telegram: https://t.me/missuo"
 		;;
 	2)
 		clear
 		echo
 		echo "你选择了增加IPV4的模式"
 		echo
-			
 		echo "开始为该模式配置"
-		echo
 		sed -i '/AllowedIPs = ::/d'  wgcf-profile.conf
 		sed -i 's/'engage.cloudflareclient.com'/'[2606:4700:d0::a29f:c001]'/g' wgcf-profile.conf
 		echo
@@ -109,7 +146,7 @@ get_char(){
 		echo
 		echo "开始安装WireGuard"
 		echo 
-		yum install kmod-wireguard wireguard-tools -y
+		check_system
 		echo 
 		echo "安装完成，开始加载WireGuard内核模块"
 		modprobe wireguard
@@ -125,25 +162,22 @@ get_char(){
 		echo
 		echo "恭喜你！配置完成！"
 		echo
-		echo "Made by Vincent"
+		echo "感谢使用!"
 		echo
-		echo "Telegram: https://t.me/missuo"
 		;;
 	3)
 		clear
 		echo
 		echo "你选择了增加IPV4和IPV6的模式"
 		echo
-			
 		echo "开始为该模式配置"
-		echo
 		sed -i 's/'engage.cloudflareclient.com'/'162.159.192.1'/g' wgcf-profile.conf
 		echo
 		echo "修改配置完成"
 		echo
 		echo "开始安装WireGuard"
 		echo 
-		yum install kmod-wireguard wireguard-tools -y
+		check_system
 		echo 
 		echo "安装完成，开始加载WireGuard内核模块"
 		modprobe wireguard
@@ -162,9 +196,8 @@ get_char(){
 		echo
 		echo "恭喜你！配置完成！"
 		echo
-		echo "Made by Vincent"
+		echo "感谢使用!"
 		echo
-		echo "Telegram: https://t.me/missuo"
 		;;
 
 	*)
