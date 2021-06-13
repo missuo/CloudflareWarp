dff4e47105c34cd5ef9671aafca3b22d91b4a677
Vincent Young <u@nisekoo.com>
2021-06-13 20:57:05 +0800

    Update warp.sh

--- warp.sh
+++ warp.sh
@@ -94,6 +94,7 @@ check_sys(){
 		echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable-wireguard.list
 		printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' > /etc/apt/preferences.d/limit-unstable
 		apt-get install linux-headers-`uname -r` -y
+		apt-get update
 		apt-get install wireguard-dkms wireguard-tools -y
 	elif [ $release = "Ubuntu" ]
 	then
